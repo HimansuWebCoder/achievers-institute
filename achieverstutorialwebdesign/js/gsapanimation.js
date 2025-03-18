0ead84c5bef2ff8e66826cea32228134a8a8b7e3
himansu naik <naikhimansu546@gmail.com>
2025-03-18 19:14:54 +0530

    add gsap animation scroll

---
+++ achieverstutorialwebdesign/js/gsapanimation.js
@@ -0,0 +1,17 @@
+// Register ScrollTrigger plugin
+		gsap.registerPlugin(ScrollTrigger);
+
+		// Animate each box on scroll
+		gsap.utils.toArray(".box").forEach(box => {
+		    gsap.to(box, {
+		        opacity: 1,
+		        y: 0,
+		        duration: 1.2,
+		        ease: "power2.out",
+		        scrollTrigger: {
+		            trigger: box,
+		            start: "top 80%",  // Trigger when box enters 80% of viewport
+		            toggleActions: "play none none reverse"
+		        }
+		    });
+		});

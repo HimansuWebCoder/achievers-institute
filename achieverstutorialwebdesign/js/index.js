7ac53b314c75f6c518026b1c41c29acc064c80da
himansu naik <naikhimansu546@gmail.com>
2025-03-18 11:14:29 +0530

    updated image slides

--- achieverstutorialwebdesign/js/index.js
+++ achieverstutorialwebdesign/js/index.js
@@ -21,3 +21,45 @@ document.querySelectorAll('.faq-question').forEach(item => {
                 answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
             });
         });
+
+
+
+// Images Slider
+const nextEl = document.querySelector(".next");
+
+const prevEl = document.querySelector(".prev");
+
+const imgsEl = document.querySelectorAll(".image-slides");
+
+const imageContainerEl = document.querySelector(".image-container");
+
+let currentImg = 1;
+
+let timeout;
+
+nextEl.addEventListener("click", () => {
+  currentImg++;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+prevEl.addEventListener("click", () => {
+  currentImg--;
+  clearTimeout(timeout);
+  updateImg();
+});
+
+updateImg();
+
+function updateImg() {
+  if (currentImg > imgsEl.length) {
+    currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = imgsEl.length;
+  }
+  imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
+}

429dc160154f924582bf47581feb726e51be0644
himansu naik <naikhimansu546@gmail.com>
2025-03-20 21:59:23 +0530

    updated faq section

--- achieverstutorialwebdesign/js/index.js
+++ achieverstutorialwebdesign/js/index.js
@@ -31,6 +31,26 @@ document.querySelectorAll('.faq-question').forEach(item => {
             });
         });
 
+const buttons = document.querySelectorAll(".card-item-btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        this.classList.toggle("active");
+        const descriptions = this.nextElementSibling;
+        const plusIcons = this.querySelector(".plus-icon");
+        const minusIcons = this.querySelector(".minus-icon");
+
+        if(descriptions.style.maxHeight){
+            descriptions.style.maxHeight = null;
+            plusIcons.style.display = "block";
+            minusIcons.style.display = "none";
+        }else{
+            descriptions.style.maxHeight = descriptions.scrollHeight + 'px';
+            plusIcons.style.display = 'none';
+            minusIcons.style.display = 'block';
+        }
+    });
+});
 
 
 // Images Slider

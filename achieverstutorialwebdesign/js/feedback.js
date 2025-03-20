ba2f491fbe46eea975e809283bd8a64fbc2e0a4a
himansu naik <naikhimansu546@gmail.com>
2025-03-20 06:17:48 +0530

    updated faq section

--- achieverstutorialwebdesign/js/feedback.js
+++ achieverstutorialwebdesign/js/feedback.js
@@ -1,8 +1,8 @@
- let testimonials = document.querySelectorAll(".feedback-sub-container");
-        let index = 0;
+ // let testimonials = document.querySelectorAll(".feedback-sub-container");
+ //        let index = 0;
 
-        setInterval(() => {
-            testimonials[index].classList.remove("active");
-            index = (index + 1) % testimonials.length;
-            testimonials[index].classList.add("active");
-        }, 3000);
+ //        setInterval(() => {
+ //            testimonials[index].classList.remove("active");
+ //            index = (index + 1) % testimonials.length;
+ //            testimonials[index].classList.add("active");
+ //        }, 3000);
